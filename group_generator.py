ec660aea281d1a4a968f08439b360b92393c757a
galandras13 <galandras13@gmail.com>
2021-10-26 21:12:50 +0200

    Implement main logic for group generation
    Implement the following functions:
     - had_common_group
     - get_all_possible_groups
     - generate_groups
    
    Modify the following functions:
     - get_previous_groups_from_file
     - get_all_students
     - get_group_size
    
    Remove the following functions:
     - get_number_of_students
     - is_valid_group_size
    
    Add import for itertools module

--- group_generator.py
+++ group_generator.py
@@ -1,74 +1,70 @@
+import itertools
+
+
 INPUTFILE = "SPOILER_test_input.txt"
 
 
-def get_prevoius_groups_from_file(file_name=INPUTFILE):
+def get_prevoius_groups_from_file(file_name):
     file = open(file_name, 'r')
     previous_groups = [group.replace('\n', '').split(', ') for group in file.readlines()]
     file.close()
     return previous_groups
 
 
-def get_all_students():
-    previous_groups = get_prevoius_groups_from_file()
+def get_all_students(previous_groups):
     students = set()
     for group in previous_groups:
         for student in group:
             students.add(student)
-    return list(students)
+    return sorted(list(students))
 
 
-def get_number_of_students():
-    students = get_all_students()
-    return len(students)
+def had_common_group(previous_groups, possible_group):
+    for i in range(len(possible_group) - 1):
+        for j in range(i + 1, len(possible_group)):
+            for group in previous_groups:
+                if possible_group[i] in group and possible_group[j] in group:
+                    return True
+    return False
 
 
-def is_valid_group_size(group_size: str):
-    if not group_size.isnumeric() or int(group_size) < 2 or int(group_size) > get_number_of_students():
-        return False
-    return True
-
-
-def get_all_possible_groups(group_size):
-    students = get_all_students()
-    groups = []
-    for i in range(len(students) - group_size + 1):
-        group = []
-        for j in range(group_size):
-            group.append(students[i])
-        groups.append(group)
-    return groups
+def get_all_possible_groups(students, group_size):
+    return [[student for student in group] for group in itertools.combinations(students, group_size)]
 
 
 def generate_groups(file, group_size):
-    previous_groups = get_prevoius_groups_from_file()
-    students = get_all_students()
-    while len(students) > 0:
-        for group in previous_groups:
-            pass
-        students.pop(0)
+    previous_groups = get_prevoius_groups_from_file(file)
+    students = get_all_students(previous_groups)
 
-    # TODO:
-    # -- check if any two people teamed before in a group
-    # -- generete every possible groups from students of groups_size
-    # - loop through every variation of students
-    # - check if they were in the same group previously
-    # - if not, create a new group of them
-    # - do this for group_size sized groups
-    pass
+    while group_size > len(students):
+        print(f"There are not enough students for {group_size} sized groups!")
+        group_size = get_group_size()
+
+    possible_groups = get_all_possible_groups(students, group_size)
+    optimal_groups = []
+    for possible_group in possible_groups:
+        if not had_common_group(previous_groups, possible_group):
+            optimal_groups.append(possible_group)
+    return optimal_groups
 
 
 def get_group_size():
     group_size = input("Enter a group size:\n")
-    while not is_valid_group_size(group_size):
-        group_size = input("Invalid group size!\nPlease enter a positive integer:\n")
+    while not group_size.isnumeric() or int(group_size) < 2:
+        group_size = input("Invalid group size!\nPlease enter an integer greater than 1:\n")
     return int(group_size)
 
 
 def main():
-    group_size = get_group_size()
-    print(group_size)
+    # group_size = get_group_size()
     # generate_groups('test_.txt', group_size)
-    generate_groups(INPUTFILE, group_size)
+
+    print(generate_groups(INPUTFILE, 3))
+    
+    # TODO:
+    # - generate different variations of optimal groups
+    #   of all students if possible
+    # - write the result(s) into file(s)
 
 
 if __name__ == '__main__':
